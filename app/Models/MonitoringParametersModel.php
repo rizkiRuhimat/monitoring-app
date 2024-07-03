517bb8500b9e870b425754606c6fb596ea3b8d2d
rizkiRuhimat <rizki.ruhimat@gmail.com>
2024-07-03 19:13:25 +0700

    parammodel

--- app/Models/MonitoringParametersModel.php
+++ app/Models/MonitoringParametersModel.php
@@ -7,8 +7,7 @@ class MonitoringParametersModel extends Model
 {
     protected $table = 'monitoring_parameters';
     protected $allowedFields = [
-        'parameter_name', 'threshold', 'kpi_indicator', 'description',
-        'hostname', 'ip_address', 'service_name', 'created_at', 'updated_at'
+        'monitoring_tool','ip_address','name_server','functional_server','services','ports_service','resources','thresholds','kpi_indicator','tags','description','created_at','updated_at'
     ];
     protected $useTimestamps = true;
 }
