3b2b97b4016d0e883773a1e23beb080f42510583
rizkiRuhimat <rizki.ruhimat@gmail.com>
2024-07-04 13:38:03 +0700

    df

--- app/Views/monitoring_parameters/edit.php
+++ app/Views/monitoring_parameters/edit.php
@@ -12,7 +12,7 @@ Edit Monitoring Parameter
     <div class="form-group">
         <label for="monitoring_tool">Monitoring Tool</label>
         <?php foreach ($tools_names as $tools): ?>
-                <option value="<?= $tools ?>" <?= $parameter['monitoring_tool'] == $tools ? 'selected' : '' ?><?= $tools ?></option>
+            <option value="<?= $tools ?>" <?= $parameter['monitoring_tool'] == $tools ? 'selected' : '' ?>><?= $tools ?></option>
             <?php endforeach; ?>
         </select>
     </div>
