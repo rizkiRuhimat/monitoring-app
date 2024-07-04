d9d81c0772bd3dbf7b233a4af059c2cb27bf2fd6
rizkiRuhimat <rizki.ruhimat@gmail.com>
2024-07-04 09:31:33 +0700

    update monitoringparameteModel

--- app/Models/MonitoringParametersModel.php
+++ app/Models/MonitoringParametersModel.php
@@ -5,9 +5,11 @@ use CodeIgniter\Model;
 
 class MonitoringParametersModel extends Model
 {
-    protected $table = 'monitoring_parameters';
+    protected $table = 'monitoring_parameter';
     protected $allowedFields = [
-        'monitoring_tool','ip_address','name_server','functional_server','services','ports_service','resources','thresholds','kpi_indicator','tags','description','created_at','updated_at'
+        'monitoring_tool','ip_address','name_server','functional_server',
+        'services','ports_service','resources','thresholds',
+        'kpi_indicator','tags','description','created_at','updated_at'
     ];
     protected $useTimestamps = true;
 }
